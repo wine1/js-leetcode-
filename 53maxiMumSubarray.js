4633eb7383e6addc5858c475814848f1d9b514eb
tianjingjing1 <jingjing1@eastmoney.com>
2019-10-12 17:02:35 +0800

    add3

---
+++ 53maxiMumSubarray.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=53 lang=javascript
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    var max;
+    var sum;
+    for(var i=0;i<nums.length;i++) {
+        for(var j=i;j<nums.length;j++) {
+            sum[j]=dum[j]+nums[j];
+        }
+    }
+};
---
+++ 66plusOne.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=66 lang=javascript
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+};
+// @lc code=end
+
