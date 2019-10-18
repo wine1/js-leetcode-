45529aea9fe8141b049033dc8b5ad63926768110
tianjingjing1 <jingjing1@eastmoney.com>
2019-10-18 21:10:12 +0800

    今天的都是二叉树，一个没写出来

---
+++ 88mergeSortArray.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=88 lang=javascript
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+let merge = (nums1, m, nums2, n) => {
+
+    for(let i=0;i<m;i++) {
+        for(let j=0;j<n;j++) {
+            if(nums1[i]>nums2[j]){
+                
+            }
+        }
+    }
+
+
+};
+
+log(merge([1,2,3,0,0,0],3,[2,5,6],3));
+// @lc code=end
