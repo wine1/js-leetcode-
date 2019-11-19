c375f84888973a42c1dbe5bfe7ec4544047c517f
tianjingjing1 <jingjing1@eastmoney.com>
2019-11-19 17:33:26 +0800

    add

---
+++ 367validPerfectSquare.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=367 lang=javascript
+ *
+ * [367] Valid Perfect Square
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    let temp;
+    if(num===0) {
+        return false;
+    }else {
+        
+    }
+};
+// @lc code=end
+
+log(isPerfectSquare(16));
+log(isPerfectSquare(14));
