a57abb4433fc67a1d25f74dd1495192573a5dc8e
tianjingjing1 <jingjing1@eastmoney.com>
2019-11-22 22:17:56 +0800

    modify

---
+++ 387FirstUniqueCharacterinaString.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=387 lang=javascript
+ *
+ * [387] First Unique Character in a String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+
+};
+// @lc code=end
+// log(firstUniqChar('leetcode'));
+// log(firstUniqChar('loveleetcode'));
+log(firstUniqChar("cc"));
---
+++ 392IsSubsequence.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=392 lang=javascript
+ *
+ * [392] Is Subsequence
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let arr1=s.split('');
+    let arr2=t.split('');
+    let len1=s.length;
+    let len2=t.length;
+   
+};
+// @lc code=end
+
+log(isSubsequence("abc","ahbgdc"));
+log(isSubsequence("axc","ahbgdc"));
+
