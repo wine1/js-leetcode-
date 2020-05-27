0ad60a322c38a8d15bec441c93c95ebf927d0ccd
tianjingjing1 <jingjing1@eastmoney.com>
2020-05-27 08:56:10 +0800

    add 226

---
+++ 226. 翻转二叉树.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=javascript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+
+//  递归交换左右子树
+
+var invertTree = function(root) {
+    if (root === null) {
+      return null;
+    }
+    let left = invertTree(root.left);
+    let right = invertTree(root.right);
+    root.left = right;
+    root.right = left;
+    return root;
+  };
+  // @lc code=end
+  
