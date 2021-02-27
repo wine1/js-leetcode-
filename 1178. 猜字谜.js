a7b541697724733ec102fbcd2489482e2f3ee946
tianjingjing1 <jingjing1@eastmoney.com>
2021-02-27 22:05:50 +0800

    modify

---
+++ 1178. 猜字谜.js
@@ -0,0 +1,36 @@
+// https://leetcode-cn.com/problems/number-of-valid-words-for-each-puzzle/
+
+/**
+ * @param {string[]} words
+ * @param {string[]} puzzles
+ * @return {number[]}
+ */
+ var findNumOfValidWords = function (words, puzzles) {
+    let resArray = new Array
+    let m = puzzles.length
+    let n=words.length
+    // let newWords=new Array
+    for(let i=0;i<n;i++) {
+        let str =  [].filter.call(words[i],(s,i,o)=>o.indexOf(s)==i).join(''); 
+        // newWords.push(str)
+        for(let s=0;s<str.length;s++) {
+            let temp=str[s]
+            for(let j=0;j<m;j++) {
+                
+            }
+        }
+     
+
+    }
+    // console.log('newWords',newWords)
+    for (let i = 0; i < m; i++) {
+        let first = puzzles[i].slice(0,1)
+
+    }
+    return resArray
+};
+
+console.log(findNumOfValidWords["aaaa", "asas", "able", "ability", "actt", "actor", "access"], ["aboveyz", "abrodyz", "abslute", "absoryz", "actresz", "gaswxyz"])//[1,1,3,2,4,0]
+// ["aaaa","asas","able","ability","actt","actor","access"]
+// ["aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"]
+console.log(111)
---
+++ 395. 至少有 K 个重复字符的最长子串.js
@@ -0,0 +1,61 @@
+// https://leetcode-cn.com/problems/longest-substring-with-at-least-k-repeating-characters/
+// 给你一个字符串 s 和一个整数 k ，请你找出 s 中的最长子串， 要求该子串中的每一字符出现次数都不少于 k 。返回这一子串的长度。
+// 示例 1：
+
+// 输入：s = "aaabb", k = 3
+// 输出：3
+// 解释：最长子串为 "aaa" ，其中 'a' 重复了 3 次。
+// 示例 2：
+
+// 输入：s = "ababbc", k = 2
+// 输出：5
+// 解释：最长子串为 "ababb" ，其中 'a' 重复了 2 次， 'b' 重复了 3 次。
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+ var longestSubstring = function(s, k) {
+    const n = s.length;
+    return dfs(s, 0, n - 1, k);
+}
+
+const dfs = (s, l, r, k) => {
+    const cnt = new Array(26).fill(0);
+    for (let i = l; i <= r; i++) {
+        cnt[s[i].charCodeAt() - 'a'.charCodeAt()]++;
+    }
+
+    let split = 0;
+    for (let i = 0; i < 26; i++) {
+        if (cnt[i] > 0 && cnt[i] < k) {
+            split = String.fromCharCode(i + 'a'.charCodeAt());
+            break;
+        }
+    }
+    if (split == 0) {
+        return r - l + 1;
+    }
+
+    let i = l;
+    let ret = 0;
+    while (i <= r) {
+        while (i <= r && s[i] === split) {
+            i++;
+        }
+        if (i > r) {
+            break;
+        }
+        let start = i;
+        while (i <= r && s[i] !== split) {
+            i++;
+        }
+
+        const length = dfs(s, start, i - 1, k);
+        ret = Math.max(ret, length);
+    }
+    return ret;
+};
+console.log(longestSubstring("aaabb",3))
+console.log(longestSubstring("ababbc",2))
