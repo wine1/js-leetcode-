c041ff51f9c91a9e8dea597bcafbb53ecaffc71a
tianjingjing1 <jingjing1@eastmoney.com>
2021-03-08 14:38:08 +0800

    modigy

---
+++ 131. 分割回文串.js
@@ -0,0 +1,26 @@
+// https://leetcode-cn.com/problems/palindrome-partitioning/
+
+// 给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。
+
+// 回文串 是正着读和反着读都一样的字符串。
+
+// 示例 1：
+
+// 输入：s = "aab"
+// 输出：[["a","a","b"],["aa","b"]]
+// 示例 2：
+
+// 输入：s = "a"
+// 输出：[["a"]]
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+
+// 回溯+动态规划
+
+ var partition = function(s) {
+
+};
+
+console.log(partition("aab"))
