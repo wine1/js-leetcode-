31d488ce7724b5db3bcbf14cb86dcca8a0aa839c
tianjingjing1 <jingjing1@eastmoney.com>
2021-03-10 20:02:14 +0800

    modify

--- 131. 分割回文串.js
+++ 131. 分割回文串.js
@@ -19,8 +19,48 @@
 
 // 回溯+动态规划
 
- var partition = function(s) {
+var partition = function(s) {
+    let res=[]
 
+    const dp = new Array(s.length);
+    for (let i = 0; i < dp.length; i++) {
+        dp[i] = new Array(s.length);
+    }
+    for (let j = 0; j < s.length; j++) {
+        for (let i = 0; i <= j; i++) {
+            if (i == j) {
+                dp[i][j] = true;
+            } else if (j - i == 1 && s[i] == s[j]) {
+                dp[i][j] = true;
+            } else if (j - i > 1 && s[i] == s[j] && dp[i + 1][j - 1]) {
+                dp[i][j] = true;
+            } else {
+                dp[i][j] = false;
+            }
+        }
+    }
+    function dfs(temp,start) {
+        if(start === s.length) {
+
+            console.log('restemp',temp)
+            res.push(temp.slice())
+            return
+        }
+        for(let i=start;i<s.length;i++) {
+            if (dp[start][i]) {
+                temp.push(s.substring(start, i + 1));
+                dfs(temp, i + 1);
+                temp.pop();
+            }
+        }
+    }
+
+    dfs([],0)
+    return res
 };
 
-console.log(partition("aab"))
+
+
+
+console.log(partition("aabba"))
+console.log(1)
---
+++ 132. 分割回文串 II.js
@@ -0,0 +1,22 @@
+// https://leetcode-cn.com/problems/palindrome-partitioning-ii/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+//  给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是回文。
+
+//  返回符合要求的 最少分割次数 。
+ var minCut = function(s) {
+    let res=[]
+    function dfs(temp,n) {
+        if(s.length===n){
+            res.push(temp)
+        }else{
+            
+        }
+    }
+
+    dfs[0]
+};
+
+console.log(minCut('aabba'))
